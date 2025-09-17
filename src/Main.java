1c69982c8592d6037bc8f8515d3b64a4807e1171
chantal <chantaluwitonze6@gmail.com>
2025-09-17 23:16:37 +0200

    Implemented ATM deposit methods: cash,check,and mobile transfer

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         ATM atm = new ATM();
         atm.depositCash(100);
+        ATM atm1 = new ATM();
+        atm1.depositCheck(100,"12BK1233");
+        ATM atm2 = new ATM();
+        atm2.depositMobileTransfer("0788048876",100);
+
 
 
 
